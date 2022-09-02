33aa9a6b4817451652de06dc80663abad908b8bb
Dh T <dhruvint@ultimateinfoways.com>
2022-09-02 07:35:51 +0530

    Added Unit Test

--- DTCodeChallengeTests/DTCodeChallengeTests.swift
+++ DTCodeChallengeTests/DTCodeChallengeTests.swift
@@ -7,8 +7,11 @@
 
 import XCTest
 @testable import DTCodeChallenge
+import Combine
 
 class DTCodeChallengeTests: XCTestCase {
+    
+    var cancellables = Set<AnyCancellable>()
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -24,6 +27,33 @@ class DTCodeChallengeTests: XCTestCase {
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+    
+    func test_dataResult_shouldBeEmpty_whenLoadFirstTime() throws {
+        let vm = DataViewModel()
+        
+        XCTAssertTrue(vm.dataResult.isEmpty)
+        XCTAssertEqual(vm.dataResult.count, 0)
+    }
+    
+    func test_dataResult_shouldNotBeEmpty_afterAPICall() throws {
+        //Given
+        let vm = DataViewModel()
+        
+        //when
+        let expectation = XCTestExpectation(description: "Should return items after few seconds")
+        
+        vm.$dataResult
+            .dropFirst()
+            .sink { items in
+            expectation.fulfill()
+            }.store(in: &cancellables)
+        
+        vm.getResultData()
+        
+        //Then
+        wait(for: [expectation], timeout: 5)
+        XCTAssertGreaterThan(vm.dataResult.count, 0)
     }
 
     func testPerformanceExample() throws {
